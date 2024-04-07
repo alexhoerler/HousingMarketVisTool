4040a0008cc52caba51a6dbee5f49ccd707a2b30
Alex Hoerler <alex.hoerler@gatech.edu>
2024-04-06 20:44:32 -0400

    Save trained model

--- pricePredictor/predictorModel.py
+++ pricePredictor/predictorModel.py
@@ -5,22 +5,23 @@ import torch.nn as nn
 class PricePredictor(nn.Module):
     def __init__(
         self,
-        d_model=11
+        d_model=64
     ):
         super(PricePredictor, self).__init__()
         self.d_model = d_model
 
         self.dropout = nn.Dropout()
+        self.in_linear = nn.Linear(self.d_model + 11, self.d_model)
         self.gru = nn.GRU(1, self.d_model)
-        self.linear = nn.Linear(self.d_model, 1)
+        self.out_linear = nn.Linear(self.d_model, 1)
 
     def forward(self, x, hidden_state, added_features):
-        hidden_state = self.dropout(hidden_state)
         added_features = self.dropout(added_features)
-        used_hidden = hidden_state + added_features
+        used_hidden = torch.cat((hidden_state, added_features), dim=-1)
 
-        gru_out, _ = self.gru(x, used_hidden)
+        gru_hidden = self.in_linear(used_hidden)
+        gru_out, _ = self.gru(x, gru_hidden)
         gru_hidden = gru_out[-1, :]
-        prediction = self.linear(gru_hidden)
+        prediction = self.out_linear(gru_hidden)
 
-        return prediction, gru_hidden
+        return prediction, gru_hidden.unsqueeze(0)
--- pricePredictor/trainModel.py
+++ pricePredictor/trainModel.py
@@ -1,10 +1,11 @@
+import os
 import torch
 from torch.utils.data import DataLoader
 from dataset import PricePredictorDataset
 from predictorModel import PricePredictor
 
 
-def createDataloader(train_percentage=0.95):
+def createDataloader(train_percentage=0.97):
     house_dataset = PricePredictorDataset("../data/data.csv")
     train_size = int(train_percentage * len(house_dataset))
     eval_size = len(house_dataset) - train_size
@@ -18,18 +19,78 @@ def createDataloader(train_percentage=0.95):
 
 
 def trainTest(model, criterion, optimizer, train_loader, test_loader):
+    train_loss = 0
     for train_batch in train_loader:
         data = train_batch[0][0]
         targets = train_batch[1][0]
+
+        inputs = data[:, 0:1]
+        added_features = data[:, 1:]
+
+        for iteration in range(inputs.shape[0] - 1):
+            iteration_input = inputs[:iteration + 1, :]
+            iteration_features = added_features[:iteration + 1, :]
+            iteration_target = targets[iteration].unsqueeze(0)
+
+            optimizer.zero_grad()
+            hidden_state = torch.tensor([[0.0] * model.d_model])
+            prediction = None
+            for row in range(iteration_input.shape[0]):
+                current_input = iteration_input[row, :].unsqueeze(0)
+                current_features = iteration_features[row, :].unsqueeze(0)
+                prediction, hidden_state = model(
+                    current_input, hidden_state, current_features)
+            loss = criterion(prediction, iteration_target)
+            train_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+    avg_train_loss = int(train_loss / (len(train_loader) * 9))
+    print("Average train loss: ", avg_train_loss)
+
+    test_loss = 0
+    for test_batch in test_loader:
+        data = test_batch[0][0]
+        targets = test_batch[1][0]
+
+        inputs = data[:, 0:1]
+        added_features = data[:, 1:]
+
+        target = targets[-2].unsqueeze(0)
+        hidden_state = torch.tensor([[0.0] * model.d_model])
+        prediction = None
+        for row in range(inputs.shape[0] - 1):
+            current_input = iteration_input[row, :].unsqueeze(0)
+            current_features = iteration_features[row, :].unsqueeze(0)
+            prediction, hidden_state = model(
+                current_input, hidden_state, current_features)
+        print(
+            f"Actual avg_mean_ppsf: {target[0].item()}, Predicted avg_mean_ppsf: {prediction[0].item()}")
+        loss = criterion(prediction, target)
+        test_loss += loss.item()
+
+    avg_test_loss = int(test_loss / len(test_loader))
+    print("Average tests loss: ", avg_test_loss)
+
+    return avg_train_loss, avg_test_loss
 
 
 if __name__ == "__main__":
     train_data_loader, eval_data_loader = createDataloader()
 
     model = PricePredictor()
+    if os.path.exists("predictor.pth"):
+        print("Loading model from file")
+        model.load_state_dict(torch.load("predictor.pth"))
+
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters())
-    epochs = 20
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+    epochs = 5000
     for epoch in range(epochs):
-        trainTest(model, criterion, optimizer,
-                  train_data_loader, eval_data_loader)
+        print("Epoch: ", epoch)
+        avg_train_loss, avg_test_loss = trainTest(model, criterion, optimizer,
+                                                  train_data_loader, eval_data_loader)
+        if avg_train_loss < 6500000 and avg_test_loss < 1000000:
+            print("Local minima found; Breaking")
+            break
+    torch.save(model.state_dict(), "predictor.pth")
